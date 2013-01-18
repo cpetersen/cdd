343b7f20818e469355ee3ee96f5818a4d0ec1e5d
Christopher Petersen <christopher.petersen@gmail.com>
2013-01-18 11:37:09 -0800

    Got rid of an extra function

--- lib/cdd/project.rb
+++ lib/cdd/project.rb
@@ -2,13 +2,5 @@ module CDD
   class Project < Base
     attr_accessor :name
     attr_accessor :vault
-
-    def projects
-      client.execute(projects_url)
-    end
-
-    def projects_url
-      "/api/v1/vaults/#{self.id}/projects"
-    end
   end
 end
